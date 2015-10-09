499899e4968f2aff11c6ca37851b338cde211659
Matthew F. Short <short.matthew.f@gmail.com>
2015-10-09 08:41:45 -0400

    remove express entirely

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-var express = require('express'),
-    server  = require('./lib/initialize')(express),
-    PORT    = process.env.port || 3000,
-    mongoose = require('mongoose'),
-    MONGOURI = process.env.mongouri || "mongodb://localhost/user-login-demo";
+/* import necessary libraries */
+var mongoose = require('mongoose'),
+    server   = require('./lib/create-server')();
 
+/* set universal constants */
+var MONGOURI = process.env.mongouri || "mongodb://localhost/user-login-demo",
+    PORT     = process.env.port || 3000;
+
+/* include routers for specific routes */
 server.use('/session', require('./controllers/session'));
 server.use('/users', require('./controllers/users'));
 
+/* define other routes */
 server.get('/', function (req, res) {
   res.render('welcome');
 });
 
+/* spin it up */
 mongoose.connect(MONGOURI);
 server.listen(PORT, function () {
   console.log("I'm up on port", PORT);
